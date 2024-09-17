47d1d96a977f2c76619e2a0807f260e36b97eb00
qingmu <xiaoguo.jlu@gmail.com>
2024-09-18 00:50:39 +0800

    修改目录路径

--- src/main/com/qingmu/db/create_schema.sql
+++
@@ -1,4 +0,0 @@
-create user qm_admin@localhost identified by 'qm_admin@123456';
-create database qm_admin_db;
-grant create, drop, alter, select, delete, update, insert on qm_admin_db.* to qm_admin@localhost;
-
