2dd24cab7a6a687f322d8b131eb4cf8d7d044df5
qingmu <xiaoguo.jlu@gmail.com>
2025-02-10 00:49:26 +0800

    常量类命名

--- server/src/main/com/qingmu/web/infrastructure/framework/dao/po/Currency.java
+++ server/src/main/com/qingmu/web/infrastructure/framework/dao/po/Currency.java
@@ -2,9 +2,16 @@ package com.qingmu.web.infrastructure.framework.dao.po;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Currency extends BaseEntity {
-    private Long id;
-    private String currencyCode;
-    private String currencyName;
+    private Long currencyId;               // 货币ID
+    private String currencyCode;           // 货币编码
+    private String currencyName;           // 货币名称
+    private String description;            // 货币描述
+    private Integer amountPrecision;       // 金额精度
+    private Integer ratePrecision;         // 汇率精度
+    private String roundingMethod;         // 圆整方式
+    private String currencyHolidays;      // 货币节假日（存储JSON格式）
 }
